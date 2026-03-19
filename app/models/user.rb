6f83b18e2e05d856f1715d10bf0482e81dfe2289
Oscar M. Olivera <oscarmolivera@gmail.com>
2026-03-19 14:35:58 -0300

    [ BE-013] Seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  belongs_to :company
+  belongs_to :company, optional: true
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :role, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+env_seed_file = Rails.root.join("db", "seeds", "#{Rails.env}", "env_seeds.rb")
+
+puts "Seeding for environment: #{Rails.env}"
+load(env_seed_file) if File.exist?(env_seed_file)
